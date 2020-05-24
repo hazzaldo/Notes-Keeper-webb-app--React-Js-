a125aa89cc9235399ab7cfcfb854def1cb7f41b8
hazzaldo <hazzaldo@hotmail.com>
2020-05-24 23:22:09 +0100

    add the add note functionality in JS so every note is added to a state array in App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Note from './Note';
 import Footer from './Footer';
 import CreateArea from './CreateArea';
 
-function createNote(noteEntry) {
-    return (
-        <Note 
-            key={noteEntry.key}
-            title={noteEntry.title}
-            content={noteEntry.content}
-        />    
-    );
-}
-
 function App() {
+    const [notesArray, setNotesArray] = useState([]);
+
+    function addNewNote(event, noteEntry) {
+        setNotesArray(prevValue => {
+                return [...prevValue, noteEntry];
+            }
+        );
+        event.preventDefault();
+    }
+
     return (
         <div>
             <Header />
-            <CreateArea />
-            <Note key={1} title="Note title" content="Note content" />
+            <CreateArea onAdd={addNewNote}/>
+            <Note key={1} title="Note title" content="Note content"/>
             <Footer />
         </div>
     );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,33 @@
-import React from './react';
+import React, { useState } from 'react';
 
 function CreateArea(props) {
+    const [noteEntry, setNoteEntry] = useState({
+        title: "",
+        content: ""
+    });
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+
+        setNoteEntry(prevValue => {
+            return {
+                ...prevValue,
+                [name]: value
+            };
+        });
+    }
+
   return (
     <div>
-      <form>
-        <input name='title' placeholder='Title' />
-        <textarea name='content' placeholder='Take a note...' rows='3' />
-        <button>Add</button>
+      <form onSubmit={(event) => {
+                props.onAdd(event, noteEntry);
+            }}
+        >
+        <input onChange={handleChange} name='title' placeholder='Title' />
+        <textarea onChange={handleChange} name='content' placeholder='Take a note...' rows='3' />
+        <button type="submit">
+            Add
+        </button>
       </form>
     </div>
   );
