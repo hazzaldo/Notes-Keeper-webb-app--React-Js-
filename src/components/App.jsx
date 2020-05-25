075485d306b17e8623fa76b2001c8d7529b20a2f
hazzaldo <hazzaldo@hotmail.com>
2020-05-25 20:34:31 +0100

    add cubes wallpaper background

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,42 +5,40 @@ import Footer from './Footer';
 import CreateArea from './CreateArea';
 
 function App() {
-    const [notesArray, setNotesArray] = useState([]);
+  const [notesArray, setNotesArray] = useState([]);
 
-    function addNewNote(noteEntry) {
-        setNotesArray(prevValue => {
-                return [...prevValue, noteEntry];
-            }
+  function addNewNote(noteEntry) {
+    setNotesArray(prevValue => {
+      return [...prevValue, noteEntry];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotesArray(prevValue => {
+      return notesArray.filter((note, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNewNote} />
+      {notesArray.map((note, index) => {
+        return (
+          <Note
+            onDelete={deleteNote}
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+          />
         );
-    }
-
-    function deleteNote(id) {
-        setNotesArray(prevValue => {
-            return (notesArray.filter((note, index) => {
-                    return index !== id
-                })
-            )
-        })
-    }
-
-    return (
-        <div>
-            <Header />
-            <CreateArea onAdd={addNewNote}/>
-            {notesArray.map((note, index) => {
-                return (
-                    <Note
-                        onDelete={deleteNote} 
-                        key={index} 
-                        id={index} 
-                        title={note.title} 
-                        content={note.content} 
-                    />
-                );   
-            })}
-            <Footer />
-        </div>
-    );
+      })}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
