484b182bef4d5a2101f5224a446bcb261e4c854f
hazzaldo <hazzaldo@hotmail.com>
2020-05-25 22:30:44 +0100

    Updates

---
+++ precache-manifest.9fabf98f610813aa18f227dc81a0ffd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25f90c298d9d4d4499859f91b3bc0b63",
+    "url": "/Notes-Keeper-webb-app--React-Js-/index.html"
+  },
+  {
+    "revision": "ef8aaa393a27237bc208",
+    "url": "/Notes-Keeper-webb-app--React-Js-/static/css/main.b9b694ed.chunk.css"
+  },
+  {
+    "revision": "8e206b3f8a82ee5c87c7",
+    "url": "/Notes-Keeper-webb-app--React-Js-/static/js/2.4acd22f8.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/Notes-Keeper-webb-app--React-Js-/static/js/2.4acd22f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef8aaa393a27237bc208",
+    "url": "/Notes-Keeper-webb-app--React-Js-/static/js/main.94ee36e3.chunk.js"
+  },
+  {
+    "revision": "d4b421e1b89248890e33",
+    "url": "/Notes-Keeper-webb-app--React-Js-/static/js/runtime-main.9119cb31.js"
+  }
+]);
